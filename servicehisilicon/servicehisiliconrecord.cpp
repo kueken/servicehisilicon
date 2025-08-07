e683044bd735d8ab01f28c28cd1bbd5f334d5090
kueken <ohnemailadresse@arcor.de>
2025-08-07 14:03:08 +0200

    Rework header

--- servicehisilicon/servicehisiliconrecord.cpp
+++ servicehisilicon/servicehisiliconrecord.cpp
@@ -1,115 +1,88 @@
 #include "servicehisiliconrecord.h"
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
+#include <lib/base/estring.h>
+#include <lib/base/init.h>
+#include <lib/base/console.h>
+#include <lib/system/init.h>
 
 eServiceHisiliconRecord::eServiceHisiliconRecord(const eServiceReference &ref)
-	: m_state(stateIdle), m_error(0), m_simulate(false), m_dvr_fd(-1), m_ref(ref)
+    : m_state(stateIdle), m_error(0), m_ref(ref), m_simulate(false)
 {
 }
 
 eServiceHisiliconRecord::~eServiceHisiliconRecord()
 {
-	stop();
+    stop();
 }
 
-RESULT eServiceHisiliconRecord::connectEvent(const sigc::slot<void(iRecordableService*,int)> &event, ePtr<eConnection> &connection)
+RESULT eServiceHisiliconRecord::connectEvent(const sigc::slot<void(iRecordableService*, int)> &event, ePtr<eConnection> &connection)
 {
-	connection = new eConnection(m_event.connect(event));
-	return 0;
+    connection = new eConnection(this, m_event.connect(event));
+    return 0;
 }
 
-RESULT eServiceHisiliconRecord::prepare(const char *filename, time_t begTime, time_t endTime, int eit_event_id, const char *name, const char *descr, const char *tags, bool descramble, bool recordecm, int packetsize)
+RESULT eServiceHisiliconRecord::prepare(const char *filename, time_t begTime, time_t endTime, int eit_event_id,
+                                        const char *name, const char *descr, const char *tags, bool descramble,
+                                        bool recordecm, int packetsize)
 {
-	if (m_state != stateIdle)
-		return -1;
-
-	m_filename = filename;
-	return doPrepare();
+    m_filename = filename ? filename : "/media/hdd/default.ts";
+    m_state = statePrepared;
+    m_error = 0;
+    return 0;
 }
 
 RESULT eServiceHisiliconRecord::prepareStreaming(bool descramble, bool includeecm)
 {
-	// Streaming nicht implementiert
-	return -1;
-}
-
-int eServiceHisiliconRecord::doPrepare()
-{
-	m_dvr_fd = open("/dev/dvb/adapter0/dvr0", O_RDONLY);
-	if (m_dvr_fd < 0)
-	{
-		m_error = errno;
-		return -1;
-	}
-	m_state = statePrepared;
-	return 0;
+    m_state = statePrepared;
+    m_error = 0;
+    return 0;
 }
 
 RESULT eServiceHisiliconRecord::start(bool simulate)
 {
-	if (m_state != statePrepared)
-		return -1;
+    m_simulate = simulate;
 
-	m_simulate = simulate;
-	if (m_simulate)
-	{
-		m_state = stateRecording;
-		m_event(this, evRecordStarted);
-		return 0;
-	}
+    if (m_state != statePrepared)
+        return -1;
 
-	return doRecord();
+    int result = doRecord();
+    if (result == 0)
+    {
+        m_event(this, evRecordStarted);
+        m_state = stateRecording;
+    }
+    return result;
 }
 
 int eServiceHisiliconRecord::doRecord()
 {
-	int fd = open(m_filename.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd < 0)
-	{
-		m_error = errno;
-		return -1;
-	}
-
-	m_state = stateRecording;
-	m_event(this, evRecordStarted);
-
-	char buffer[188 * 256]; // TS packets
-	ssize_t r;
-
-	while ((r = read(m_dvr_fd, buffer, sizeof(buffer))) > 0)
-	{
-		if (write(fd, buffer, r) != r)
-			break;
-	}
-
-	close(fd);
-	m_event(this, evRecordStopped);
-	m_state = stateIdle;
-	return 0;
+    // Hier könnte man den Start der Hardware-Demux-Aufnahme triggern
+    // Momentan nur Stub
+    return 0;
 }
 
 RESULT eServiceHisiliconRecord::stop()
 {
-	if (m_state != stateRecording && m_state != statePrepared)
-		return -1;
-
-	if (m_dvr_fd >= 0)
-	{
-		close(m_dvr_fd);
-		m_dvr_fd = -1;
-	}
-
-	m_state = stateIdle;
-	m_event(this, evRecordStopped);
-	return 0;
+    if (m_state == stateRecording)
+    {
+        // Aufnahme stoppen – bei echter Implementierung Hardware-Demux beenden
+        m_state = stateIdle;
+        m_event(this, evRecordStopped);
+    }
+    return 0;
 }
 
 RESULT eServiceHisiliconRecord::stream(ePtr<iStreamableService> &ptr)
 {
-	ptr = nullptr;
-	return -1;
+    return -1;  // optional implementierbar
+}
+
+RESULT eServiceHisiliconRecord::frontendInfo(ePtr<iFrontendInformation> &ptr)
+{
+    return -1;  // optional implementierbar
+}
+
+RESULT eServiceHisiliconRecord::subServices(ePtr<iSubserviceList> &ptr)
+{
+    return -1;
 }
 
--- servicehisilicon/servicehisiliconrecord.h
+++ servicehisilicon/servicehisiliconrecord.h
@@ -3,42 +3,52 @@
 
 #include <lib/service/iservice.h>
 #include <lib/dvb/idvb.h>
-#include "servicehisilicon.h"
+#include <lib/base/messagepump.h>
+#include <lib/base/object.h>
+#include <lib/base/ebase.h>
 
-class eServiceHisiliconRecord:
-	public iRecordableService,
-	public sigc::trackable
+class eServiceHisiliconRecord :
+    public iRecordableService,
+    public sigc::trackable
 {
-	DECLARE_REF(eServiceHisiliconRecord);
+    DECLARE_REF(eServiceHisiliconRecord);
 public:
-	RESULT connectEvent(const sigc::slot<void(iRecordableService*,int)> &event, ePtr<eConnection> &connection);
-	RESULT prepare(const char *filename, time_t begTime, time_t endTime, int eit_event_id, const char *name, const char *descr, const char *tags, bool descramble, bool recordecm, int packetsize);
-	RESULT prepareStreaming(bool descramble, bool includeecm);
-	RESULT start(bool simulate=false);
-	RESULT stop();
-	RESULT stream(ePtr<iStreamableService> &ptr);
-	RESULT getError(int &error) { error = m_error; return 0; }
-	RESULT frontendInfo(ePtr<iFrontendInformation> &ptr) { ptr = 0; return -1; }
-	RESULT subServices(ePtr<iSubserviceList> &ptr) { ptr = 0; return -1; }
-	RESULT getFilenameExtension(std::string &ext) { ext = ".ts"; return 0; }
+    eServiceHisiliconRecord(const eServiceReference &ref);
+    ~eServiceHisiliconRecord();
+
+    RESULT connectEvent(const sigc::slot<void(iRecordableService*, int)> &event, ePtr<eConnection> &connection);
+    RESULT prepare(const char *filename, time_t begTime, time_t endTime, int eit_event_id,
+                   const char *name, const char *descr, const char *tags, bool descramble,
+                   bool recordecm, int packetsize);
+    RESULT prepareStreaming(bool descramble, bool includeecm);
+    RESULT start(bool simulate=false);
+    RESULT stop();
+    RESULT stream(ePtr<iStreamableService> &ptr);
+    RESULT getError(int &error) { error = m_error; return 0; };
+    RESULT frontendInfo(ePtr<iFrontendInformation> &ptr);
+    RESULT subServices(ePtr<iSubserviceList> &ptr);
+    RESULT getFilenameExtension(std::string &ext) { ext = ".ts"; return 0; };
 
 private:
-	enum { stateIdle, statePrepared, stateRecording };
+    enum {
+        stateIdle,
+        statePrepared,
+        stateRecording
+    };
 
-	int m_state;
-	int m_error;
-	bool m_simulate;
-	int m_dvr_fd;
+    enum {
+        evRecordStarted = 1,
+        evRecordStopped = 2,
+        evRecordWriteError = 3
+    };
 
-	std::string m_filename;
-	eServiceReference m_ref;
-	ePtr<eConnection> m_con_record_event;
-	sigc::signal<void(iRecordableService*,int)> m_event;
+    int m_state;
+    int m_error;
+    std::string m_filename;
+    eServiceReference m_ref;
+    bool m_simulate;
 
-	eServiceHisiliconRecord(const eServiceReference &ref);
-	~eServiceHisiliconRecord();
-	int doPrepare();
-	int doRecord();
+    sigc::signal<void(iRecordableService*, int)> m_event;
 };
 
 #endif
